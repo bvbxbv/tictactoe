aa34201066b6f875317f6dfa3114b4271ac83f67
bvbxbv <aburach@inbox.ru>
2025-11-08 12:38:07 +0700

    chore: установил и настроил eslint

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import js from "@eslint/js";
+import pluginImport from "eslint-plugin-import";
+
+export default [
+	{
+		ignores: ["node_modules", "dist", ".husky"],
+	},
+	{
+		languageOptions: {
+			ecmaVersion: "latest",
+			sourceType: "module",
+			globals: {
+				console: "readonly",
+				document: "readonly",
+				window: "readonly",
+			},
+		},
+		plugins: {
+			import: pluginImport,
+		},
+		rules: {
+			...js.configs.recommended.rules,
+			"no-unused-vars": "warn",
+			"no-console": "off",
+			"import/order": [
+				"warn",
+				{
+					groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
+					alphabetize: { order: "asc", caseInsensitive: true },
+				},
+			],
+		},
+	},
+];
