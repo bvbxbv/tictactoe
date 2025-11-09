548dedf6e0cf4eedbce2a0701128ac016caa5c21
bvbxbv <aburach@inbox.ru>
2025-11-09 14:40:30 +0700

    feat: добавил логику смены ходов

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
 import "./index.css";
+
+import { Game } from "./Game.js";
+
+const gameManager = new Game();
+const cells = document.querySelectorAll(".cell");
+
+function cellClickHandler(cell, index) {
+	const isSuccess = gameManager.makeMove(index);
+	if (isSuccess) {
+		cell.innerText = gameManager.board[index];
+	}
+}
+
+cells.forEach((cell, index) =>
+	cell.addEventListener("click", () => {
+		cellClickHandler(cell, index);
+	})
+);
