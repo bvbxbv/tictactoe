41b19d77522b4d417dc7ad1130805847c226d2a4
bvbxbv <aburach@inbox.ru>
2025-11-15 13:51:06 +0700

    refactor: создал точку входа в приложение (класс App)
    - создал прослойку App между main.js и контроллерами, моделями, вьюхами.
    - App создает BoardController и BoardView связывая их событиями.
    - убрал реализацию синглтона из BoardController.
    - теперь BoardController (равно как и BoardView) принимает зависимости, а не обращается извне.
    - изолировал BoardView. Теперь BoardView не знает ни о чём снаружи класса.

---
+++ src/core/App.js
@@ -0,0 +1,28 @@
+import { BoardController } from "../controllers/BoardController";
+import { BoardView } from "../ui/views/BoardView";
+
+import { UI } from "../ui/elements";
+import { dispatcher } from "./events/Base/EventDispatcher";
+import { logAction } from "../utils/helpers";
+import { PlayerMovedEvent } from "./events/PlayerEvents";
+
+export class App {
+	#gameManager; // FIXME: костыль. Сделал, чтобы остальные контроллеры не поломались. Пофикси
+
+	constructor(gameManager) {
+		this.#gameManager = gameManager;
+	}
+
+	boot() {
+		// TODO: переделать EventDispatcher под DI
+		const boardView = new BoardView({
+			boardDOM: UI.board,
+			onCellClick: (index) => {
+				logAction(this, PlayerMovedEvent, index);
+				dispatcher.dispatch(new PlayerMovedEvent(index));
+			},
+		});
+		const boardController = new BoardController({ gameManager: this.#gameManager, board: this.#gameManager.board, view: boardView });
+		boardController.boot();
+	}
+}
